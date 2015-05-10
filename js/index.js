d4991cd38a8639ed22fd28d8a3030231f94e1dcc
kazakovv <kazakov.vcictor@gmail.com>
2015-05-10 18:37:43 +0100

    can play video

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+window.onload = canPlayVideo;
+function canPlayVideo() { 
+	var browserCanPlayVideo=false;
+	//if not empty the browser can play it
+	if(video.canPlayType("video/mp4") != "")  { 
+		browserCanPlayVideo = true;
+	} else if (video.canPlayType("video/webm") != "") { 
+		browserCanPlayVideo = true;
+	} else if(video.canPlayType("video/ogg") != "") { 
+		browserCanPlayVideo = true;
+	}
+	
+	//ako browserCanPlayVideo is false we delete the video container and replace it with img container
+	if(browserCanPlayVideo==false) { 
+		alert("You can't play video");
+	}
+}
